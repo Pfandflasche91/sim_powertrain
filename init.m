4a04268e18f5489962d545ec043f6336ca3af833
Pfandflasche91 <max.alt@gmx.de>
2020-05-02 14:48:10 +0200

    Add files via upload
    init file for the servo and powertrain parameters

---
+++ init.m
@@ -0,0 +1,16 @@
+%%
+%STARTDATE:
+%   30.04.2020
+%CHANGES:
+%   REF No:         VERSION DATE:       WHO:        DETAIL:
+%   1               30.04.2020          MA          created
+%FILENAME:  
+%   init.m
+%DESCRIPTION:   
+%   -initialization for siemensServo.slx
+%   -includes parameters for Servo 
+%   -controll system
+%
+%%
+
+a=6;
